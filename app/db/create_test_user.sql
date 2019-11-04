1b252ef60deb3e74d8ec213394cbf0ee29b4aa4e
apm1005 <apm1005@alu.ubu.es>
2019-11-04 11:53:17 +0100

    Added permissions to user in both databases #6

--- app/db/create_test_user.sql
+++ app/db/create_test_user.sql
@@ -1,10 +1,24 @@
+USE [dev];
+GO
+
 CREATE LOGIN tfg WITH PASSWORD = '1234';
 GO
 
 CREATE USER tfg FOR LOGIN tfg;
 GO
 
-USE dev;
 EXEC sp_addrolemember 'db_datareader', 'tfg'
 EXEC sp_addrolemember 'db_datawriter', 'tfg'
 EXEC sp_addrolemember 'db_ddladmin', 'tfg'
+GO
+
+USE [test]
+GO
+
+CREATE USER tfg FOR LOGIN tfg;
+GO
+
+EXEC sp_addrolemember 'db_datareader', 'tfg'
+EXEC sp_addrolemember 'db_datawriter', 'tfg'
+EXEC sp_addrolemember 'db_ddladmin', 'tfg'
+GO
