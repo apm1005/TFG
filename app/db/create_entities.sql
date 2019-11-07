37827217c5a5c55952252544c5f0c0dcffd40179
apm1005 <apm1005@alu.ubu.es>
2019-11-07 17:27:10 +0100

    create_entities.sql script modified #10

--- app/db/create_entities.sql
+++ app/db/create_entities.sql
@@ -1,52 +1,52 @@
 CREATE TABLE Enviroment (
 	"server" VARCHAR(40) PRIMARY KEY,
-	ip_address VARCHAR(17)
+	ip_address VARCHAR(17),
 );
 
 CREATE TABLE APP (
 	id INTEGER PRIMARY KEY,
 	"name" VARCHAR(50),
 	"description" VARCHAR(100),
-	"server" VARCHAR(40) FOREIGN KEY REFERENCES Enviroment
+	"server" VARCHAR(40) FOREIGN KEY REFERENCES Enviroment,
 );
 
 CREATE TABLE EventType (
 	"type" VARCHAR(30) PRIMARY KEY,
-	"description" VARCHAR(70)
+	"description" VARCHAR(70),
 );
 
 CREATE TABLE "User" (
 	id INTEGER PRIMARY KEY,
 	name VARCHAR(50),
-	login VARCHAR(20)
-	--...
+	login VARCHAR(20),
+	--more will be added in future versions
 );
 
 CREATE TABLE ItemType (
 	"type" VARCHAR(30) PRIMARY KEY,
-	model VARCHAR(50)
+	model VARCHAR(50),
 );
 
 CREATE TABLE Item (
 	id INTEGER PRIMARY KEY,
-	"user_id" INTEGER FOREIGN KEY REFERENCES "User",
+	"user_id" INTEGER FOREIGN KEY REFERENCES "User", --NOT NULL
 	item_type VARCHAR(30) FOREIGN KEY REFERENCES ItemType,
 	ip_address VARCHAR(17),
-	mac_address VARCHAR(12)
+	mac_address VARCHAR(12),
 );
 
 CREATE TABLE "Event" (
 	id INTEGER PRIMARY KEY,
 	instant DATETIME,
 	event_type VARCHAR(30) FOREIGN KEY REFERENCES EventType,
-	"user_id" INTEGER FOREIGN KEY REFERENCES "User",
-	item_id INTEGER FOREIGN KEY REFERENCES Item,
 );
 
 CREATE TABLE "Session" (
 	id INTEGER PRIMARY KEY,
 	event_id INTEGER FOREIGN KEY REFERENCES "Event",
 	app_id INTEGER FOREIGN KEY REFERENCES APP,
+	"user_id" INTEGER FOREIGN KEY REFERENCES "User",
+	item_id INTEGER FOREIGN KEY REFERENCES Item,
 	start_time DATETIME NOT NULL,
-	end_time DATETIME
+	end_time DATETIME,
 );
