c8a8d7c4f533c21c14ff248b8dbf300335c52c40
apm1005 <apm1005@alu.ubu.es>
2019-11-17 23:21:36 +0100

    Added itemtypes app #20

---
+++ app/src/itemtypes/models.py
@@ -0,0 +1,17 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class Itemtype(models.Model):
+    type = models.CharField(primary_key=True, max_length=30)
+    model = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'ItemType'
