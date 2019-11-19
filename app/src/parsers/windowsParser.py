26dde6376453c854e0e61251470705f345b56a66
apm1005 <apm1005@alu.ubu.es>
2019-11-19 22:28:11 +0100

    Added prototype of Windows Parser #21

--- app/src/parsers/windowsParser.py
+++ app/src/parsers/windowsParser.py
@@ -1,18 +1,23 @@
+import os
+import numpy as np
+from persons.models import Person
+
+
 class WindowsParser:
     """
     Class that executes a command in Windows to extract the last login of the users.
     """
-    
-    def __get_users(self):  # TODO
+
+    def __get_users(self):
         """
         Gets the users from the database.
 
         Returns
         -------
-        list
-            a list with the login of each person
+        ndarray
+            an array with the id and login of each person
         """
-        pass
+        return np.array(Person.objects.values_list('id', 'login'), dtype='object')
 
     def __load_data(self):  # TODO
         """
@@ -23,7 +28,17 @@ class WindowsParser:
         ndarray
             an array with the event, app (Windows OS), person id, item id and timestamps
         """
-        pass
+        users = self.__get_users()
+        logons = np.empty((users.shape[0], 1), dtype=object)
+        result = np.hstack((users, logons))
+
+        for i, user in enumerate(result):
+            command = f'net user {user[1]} | findstr /B /C:"Ultima sesión iniciada"'
+            r = str(os.popen(command).read())
+            if r != '':
+                result[i, 2] = r[-22:].replace('?', '').replace('\n', '')
+
+        return result
 
     def __store_data(self):  # TODO
         """
@@ -35,4 +50,4 @@ class WindowsParser:
         """
         Parses the login of each person from Windows and stores it in the database.
         """
-        pass
+        return self.__load_data()
