4c0832f6a04a59950a79c0c9175c29edbae59e2b
apm1005 <apm1005@alu.ubu.es>
2019-11-21 14:58:26 +0100

    Changed models and created migrations #24

--- app/src/persons/models.py
+++ app/src/persons/models.py
@@ -1,18 +1,7 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 
 class Person(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, blank=True, null=True)
     login = models.CharField(max_length=20, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'Person'
