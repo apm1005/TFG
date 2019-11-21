280fbd91711d30ba01b853ea602c82a2b600f660
apm1005 <apm1005@alu.ubu.es>
2019-11-21 16:46:31 +0100

    Added apps to admin page #26

--- app/src/environments/admin.py
+++ app/src/environments/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Environment
 
-# Register your models here.
+
+admin.site.register(Environment)
--- app/src/eventtypes/admin.py
+++ app/src/eventtypes/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Eventtype
 
-# Register your models here.
+
+admin.site.register(Eventtype)
