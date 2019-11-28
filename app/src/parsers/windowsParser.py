088c4fc75e2471364f6459259a996be097f50de4
apm1005 <apm1005@alu.ubu.es>
2019-11-28 16:36:15 +0100

    Finished first version of windowsParser #31

--- app/src/parsers/windowsParser.py
+++ app/src/parsers/windowsParser.py
@@ -6,7 +6,6 @@ from persons.models import Person
 from eventtypes.models import Eventtype
 from events.models import Event
 from applications.models import App
-from items.models import Item
 from passages.models import Passage
 
 
@@ -24,7 +23,8 @@ class WindowsParser:
     Class that executes a command in Windows to extract the last login of the users.
     """
 
-    def __get_users(self):
+    @staticmethod
+    def __get_users():
         """
         Gets the users from the database.
 
@@ -35,7 +35,33 @@ class WindowsParser:
         """
         return np.array(Person.objects.values_list('id', 'login'), dtype='object')
 
-    def __load_data(self):  # TODO - Add item id to result
+    @staticmethod
+    def __set_logon_string():
+        """
+        Sets the string that will be used in the cmd command to look for last logon
+
+        Raises
+        ------
+        LanguageNotSupported
+            if the OS language is not considered
+
+        Returns
+        -------
+        string
+            an string with the info to look for last logon
+        """
+        language = locale.getdefaultlocale()[0]
+
+        if language == 'es_ES':
+            logon_string = 'Ultima'
+        elif language == 'en_US':
+            logon_string = 'Last logon'
+        else:
+            raise LanguageNotSupported('The current Windows language is not supported!')
+
+        return logon_string
+
+    def __load_data(self):
         """
         Loads the last login of each person in memory.
 
@@ -48,14 +74,7 @@ class WindowsParser:
         logons = np.empty((users.shape[0], 1), dtype=object)
         result = np.hstack((users, logons))
         date_length = 22
-        language = locale.getdefaultlocale()[0]
-
-        if language == 'es_ES':
-            logon_string = 'Ultima'
-        elif language == 'en_US':
-            logon_string = 'Last logon'
-        else:
-            raise LanguageNotSupported('The current Windows language is not supported!')
+        logon_string = self.__set_logon_string()
 
         for i, user in enumerate(result):
             user_id, user_login, _ = user
@@ -68,7 +87,8 @@ class WindowsParser:
 
         return result
 
-    def __parse_timestamp(self, timestamp):
+    @staticmethod
+    def __parse_timestamp(timestamp):
         """
         Parses the timestamp given to a format that the ORM can manage
 
@@ -85,14 +105,13 @@ class WindowsParser:
         units = datetime.datetime.strptime(timestamp, '%d/%m/%Y %H:%M:%S')
         return datetime.datetime.strftime(units, '%Y-%m-%d %H:%M:%S')
 
-    def __create_event(self, user_id, user_last_logon):
+    @staticmethod
+    def __create_event(user_last_logon):
         """
         Creates an Event in the database
 
         Parameters
         ----------
-        user_id : int
-            user identifier
         user_last_logon : timestamp
             user last logon timestamp
 
@@ -106,18 +125,19 @@ class WindowsParser:
         event.save()
         return event.id
 
-    def __create_passage(self, user_id, user_last_logon, event_id):
+    @staticmethod
+    def __create_passage(user_id, user_last_logon, event_id):
         """
         Creates a Passage in the database
         """
-        app_id = App.objects.get(name='Windows').id
-        event_id = Event.objects.get(id=event_id).id
-        # item_id  # TODO
-        person_id = Person.objects.get(id=user_id).id
-        Passage.objects.create(start_time=user_last_logon, end_time=None, app_id=app_id,
-                               event_id=event_id, item_id=None, person_id=person_id)
+        Passage.objects.create(start_time=user_last_logon,
+                               end_time=None,
+                               app_id=App.objects.get(name='Windows').id,
+                               event_id=Event.objects.get(id=event_id).id,
+                               item_id=None,
+                               person_id=Person.objects.get(id=user_id).id)
 
-    def __store_data(self):  # TODO - Add item id to passage
+    def __store_data(self):
         """
         Stores the data into the Event entity and Passage entity.
         """
@@ -126,7 +146,7 @@ class WindowsParser:
         for i, user in enumerate(users):
             user_id, user_login, user_last_logon = user
             if user_last_logon is not None:
-                event_id = self.__create_event(user_id=user_id, user_last_logon=user_last_logon)
+                event_id = self.__create_event(user_last_logon=user_last_logon)
                 self.__create_passage(user_id=user_id, user_last_logon=user_last_logon, event_id=event_id)
 
     def parse(self):
