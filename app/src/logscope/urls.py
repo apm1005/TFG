d1eca067e81621dbec67a6e35f97764d31577169
apm1005 <apm1005@alu.ubu.es>
2020-02-08 17:33:10 +0100

    Added home under construction page #40

--- app/src/logscope/urls.py
+++ app/src/logscope/urls.py
@@ -8,6 +8,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.home, name='logscope-home'),
     path('passage/', PassageListView.as_view(), name='logscope-passage'),
     path('passage/<int:pk>/', PassageDetailView.as_view(), name='passage-detail'),
     path('passage/<int:pk>/delete/', PassageDeleteView.as_view(), name='passage-delete'),
--- app/src/logscope/views.py
+++ app/src/logscope/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.views.generic import (
     ListView,
     DetailView,
@@ -6,9 +7,13 @@ from django.views.generic import (
 from .models import Passage
 
 
+def home(request):
+    return render(request, 'logscope/home.html')
+
+
 class PassageListView(ListView):
     model = Passage
-    template_name = 'logscope/home.html'
+    template_name = 'logscope/passage.html'
     context_object_name = 'passages'
     ordering = ['-start_time']
 
