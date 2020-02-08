7a7954f7d00f8db881254a3f4be989d95a09e2df
apm1005 <apm1005@alu.ubu.es>
2020-02-08 18:59:43 +0100

    Added pagination and person passages #40

--- app/src/logscope/urls.py
+++ app/src/logscope/urls.py
@@ -3,6 +3,7 @@ from .views import (
     PassageListView,
     PassageDetailView,
     PassageDeleteView,
+    PersonPassageListView,
 )
 from . import views
 
@@ -12,4 +13,5 @@ urlpatterns = [
     path('passage/', PassageListView.as_view(), name='logscope-passage'),
     path('passage/<int:pk>/', PassageDetailView.as_view(), name='passage-detail'),
     path('passage/<int:pk>/delete/', PassageDeleteView.as_view(), name='passage-delete'),
+    path('person/<str:name>', PersonPassageListView.as_view(), name='person-passages'),
 ]
--- app/src/logscope/views.py
+++ app/src/logscope/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import (
     ListView,
     DetailView,
     DeleteView,
 )
-from .models import Passage
+from .models import Passage, Person
 
 
 def home(request):
@@ -16,6 +16,18 @@ class PassageListView(ListView):
     template_name = 'logscope/passage.html'
     context_object_name = 'passages'
     ordering = ['-start_time']
+    paginate_by = 10
+
+
+class PersonPassageListView(ListView):
+    model = Passage
+    template_name = 'logscope/person_passages.html'
+    context_object_name = 'passages'
+    paginate_by = 10
+
+    def get_queryset(self):
+        person = get_object_or_404(Person, name=self.kwargs.get('name'))
+        return Passage.objects.filter(person=person).order_by('-start_time')
 
 
 class PassageDetailView(DetailView):
