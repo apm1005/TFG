f047fa56cc893919963cc9e5b056cba6131cdfca
apm1005 <apm1005@alu.ubu.es>
2020-03-12 11:37:41 +0100

    Add new fields to person #51

--- app/src/logscope/models.py
+++ app/src/logscope/models.py
@@ -3,8 +3,12 @@ from django.db import models
 
 class Person(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50, blank=True, null=True)
-    login = models.CharField(max_length=20, blank=True, null=True)
+    name = models.CharField(max_length=50, blank=True, null=False)
+    login = models.CharField(max_length=20, blank=True, null=False)
+    company = models.CharField(max_length=50, blank=True, null=False)
+    division = models.CharField(max_length=50, blank=True, null=False)
+    area = models.CharField(max_length=50, blank=True, null=False)
+    department = models.CharField(max_length=50, blank=True, null=False)
 
     def __str__(self):
         return str(self.login)
@@ -18,8 +22,8 @@ class Person(models.Model):
 class Itemtype(models.Model):
     id = models.AutoField(primary_key=True)
     type = models.CharField(max_length=30, null=False)
-    brand = models.CharField(max_length=30, blank=True, null=True)
-    model = models.CharField(max_length=50, blank=True, null=True)
+    brand = models.CharField(max_length=30, blank=True, null=False)
+    model = models.CharField(max_length=50, blank=True, null=False)
 
     def __str__(self):
         return str(self.type) + ' | ' + str(self.brand) + ' ' + str(self.model)
@@ -51,8 +55,8 @@ class Eventtype(models.Model):
 
 class Event(models.Model):
     id = models.AutoField(primary_key=True)
-    instant = models.DateTimeField(blank=True, null=True)
-    event_type = models.ForeignKey(Eventtype, models.DO_NOTHING, db_column='event_type', blank=True, null=True)
+    instant = models.DateTimeField(blank=True, null=False)
+    event_type = models.ForeignKey(Eventtype, models.DO_NOTHING, db_column='event_type', blank=True, null=False)
 
     def __str__(self):
         return str(self.instant) + ' - ' + str(self.event_type)
@@ -69,7 +73,7 @@ class Environment(models.Model):
 
 class App(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50, blank=True, null=True)
+    name = models.CharField(max_length=50, blank=True, null=False)
     description = models.CharField(max_length=100, blank=True, null=True)
     server = models.ForeignKey(Environment, models.DO_NOTHING, db_column='server', blank=True, null=True)
 
@@ -81,7 +85,7 @@ class Passage(models.Model):
     id = models.AutoField(primary_key=True)
     event = models.ForeignKey(Event, models.DO_NOTHING, null=False)
     app = models.ForeignKey(App, models.DO_NOTHING, null=False)
-    person = models.ForeignKey(Person, models.DO_NOTHING, null=True)
+    person = models.ForeignKey(Person, models.DO_NOTHING, null=False)
     item = models.ForeignKey(Item, models.DO_NOTHING, null=True)
     start_time = models.DateTimeField(null=False)
     end_time = models.DateTimeField(null=True)
