05831e719aa1e0414ff9b75e6c15aa86d192587a
apm1005 <apm1005@alu.ubu.es>
2020-03-24 00:19:49 +0100

    Edit views #55

--- app/src/logscope/views.py
+++ app/src/logscope/views.py
@@ -1,31 +1,20 @@
-from django.shortcuts import get_object_or_404
 from django.views.generic import (
-    ListView,
-    DetailView,
-    DeleteView,
     TemplateView,
 )
-from .models import App, Itemtype, Passage, Person
-from datetime import datetime
-from tfgproject.settings import PAGINATION
+from .models import Passage
 from .tables import PassageTable
 from .filters import PassageFilter
 from django_tables2.views import SingleTableMixin
+from django_tables2.export.views import ExportMixin
 from django_filters.views import FilterView
-
-
-class PassageTableView(SingleTableMixin, FilterView):
-    model = Passage
-    template_name = 'logscope/passage.html'
-    table_class = PassageTable
-    filterset_class = PassageFilter
 
 
 class HomeView(TemplateView):
     template_name = 'logscope/home.html'
 
 
-class PassageTableExport(SingleTableMixin):
+class PassageTableView(ExportMixin, SingleTableMixin, FilterView):
     model = Passage
+    template_name = 'logscope/passage.html'
     table_class = PassageTable
-    template_name = 'django_tables2/bootstrap4.html'
+    filterset_class = PassageFilter
